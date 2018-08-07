a7b7f3fcc71a76dbd233c6471ddeba96d302b86f
yangchao <yangergea@163.com>
2018-08-07 08:57:29 +0800

    fix

---
+++ painter/resources/views/painter/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>卖报的小画家</title>
+</head>
+<body>
+	<table align="center" border="100"> 
+		<tr>
+			<td>1</td>
+			<td>2</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td>4</td>
+		</tr>
+	</table>
+</body>
+</html>
